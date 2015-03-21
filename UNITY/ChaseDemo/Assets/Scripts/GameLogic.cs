81623174fb0dabde222eef20a72cdd47fbe4dcea
Peter Ren <lcdcdr2004@gmail.com>
2015-03-21 21:56:42 +0800

    Added ChaseDemo

---
+++ UNITY/ChaseDemo/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+	public Mover player;
+
+	public Mover mob;
+
+	public Text distanceText;
+
+	public float initialTime = 2.0f;
+
+	private ReadOnlyReactiveProperty<float> _distance;
+
+	void Start()
+	{
+		// Distance display
+		_distance = new ReadOnlyReactiveProperty<float>( Observable.EveryUpdate().Select( _ =>
+			Vector3.Distance( player.transform.position, mob.transform.position ) ) );
+		_distance.SubscribeToText( this.distanceText );
+
+		// Mob chase player every update
+		Observable.EveryUpdate().Subscribe( _ =>
+			mob.direction.Value = ( player.transform.position - mob.transform.position ).normalized );
+
+		// Time sequence of 1/1, 1/2, 1/3, 1/4...
+		var fracTimeSequence = Observable.Create<float>( observer =>
+		{
+			var i = 1;
+			return Scheduler.DefaultSchedulers.TimeBasedOperations.Schedule( System.TimeSpan.FromSeconds( this.initialTime / i ), self =>
+			{
+				observer.OnNext( i % 2 == 0 ? 1.0f : -1.0f );
+				i++;
+				self( System.TimeSpan.FromSeconds( this.initialTime / i ) );
+			} );
+		} );
+
+		fracTimeSequence.Subscribe( d => this.player.direction.Value = Vector3.Cross( Vector3.back, this.mob.direction.Value ).normalized * d );
+	}
+}
---
+++ UNITY/ChaseDemo/Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UniRx;
+
+public class Mover : MonoBehaviour
+{
+	public float speed = 2.0f;
+
+	public ReactiveProperty<Vector3> direction { get; private set; }
+
+	public ReadOnlyReactiveProperty<Vector3> velocity { get; private set; }
+
+	void Awake()
+	{
+		this.direction = new ReactiveProperty<Vector3>( Vector3.up );
+		this.velocity = new ReadOnlyReactiveProperty<Vector3>( this.direction.Select( dir => dir * this.speed ) );
+
+		Observable.EveryUpdate().Subscribe( _ => this.transform.Translate( this.velocity.Value * Time.deltaTime ) );
+	}
+}
